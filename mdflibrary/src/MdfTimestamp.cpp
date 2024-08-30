2a363e1c154715eb926c22705d5826dd1cc785a1
simonkimi <git@z31.mozmail.com>
2024-08-30 17:58:49 +0800

    try fix 2

--- mdflibrary/src/MdfTimestamp.cpp
+++ mdflibrary/src/MdfTimestamp.cpp
@@ -1,7 +1,7 @@
 #include "MdfTimestamp.h"
 
 namespace MdfLibrary {
-MdfUtcTimestamp::MdfUtcTimestamp(const uint64_t utc_timestamp) {
+MdfUtcTimestamp::MdfUtcTimestamp(uint64_t utc_timestamp) {
   utc_timestamp_ = utc_timestamp;
 }
 
@@ -9,7 +9,7 @@ mdf::ITimestamp* MdfUtcTimestamp::GetTimestamp() {
   return new mdf::UtcTimestamp(utc_timestamp_);
 }
 
-MdfLocalTimestamp::MdfLocalTimestamp(const uint64_t local_timestamp) {
+MdfLocalTimestamp::MdfLocalTimestamp(uint64_t local_timestamp) {
   local_timestamp_ = local_timestamp;
 }
 
@@ -17,9 +17,9 @@ mdf::ITimestamp* MdfLocalTimestamp::GetTimestamp() {
   return new mdf::LocalTimestamp(local_timestamp_);
 }
 
-MdfTimezoneTimestamp::MdfTimezoneTimestamp(const uint64_t utc_timestamp,
-                                           const int16_t timezone_offset_min,
-                                           const int16_t dst_offset_min) {
+MdfTimezoneTimestamp::MdfTimezoneTimestamp(uint64_t utc_timestamp,
+                                           int16_t timezone_offset_min,
+                                           int16_t dst_offset_min) {
   utc_timestamp_ = utc_timestamp;
   timezone_offset_min_ = timezone_offset_min;
   dst_offset_min_ = dst_offset_min;
